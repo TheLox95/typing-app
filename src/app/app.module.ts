41c6729400845f82374bf5c9c39cdc5024727345
TheLox95 <leo_9_t@hotmail.com>
2018-03-27 17:37:17 -0400

    added angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { ParagraphService } from './paragraph.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HotkeyModule} from 'angular2-hotkeys';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { ParagraphListComponent } from './paragraph-list/paragraph-list.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParagraphListComponent,
+    ParagraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    HotkeyModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ParagraphService, {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}],
+  bootstrap: [AppComponent],
+  entryComponents: [ParagraphComponent]
 })
 export class AppModule { }
